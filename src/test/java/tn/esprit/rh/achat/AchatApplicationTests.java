041671c470d01eec347744ba949e22c06e0af596
azizouachem <azizouachem0@gmail.com>
2026-05-02 17:45:34 +0100

    fix(test): force test profile to use H2 database

--- src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
+++ src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
@@ -3,7 +3,10 @@ package tn.esprit.rh.achat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class AchatApplicationTests {
 
 	@Test
